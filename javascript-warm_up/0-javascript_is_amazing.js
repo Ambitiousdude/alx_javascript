a3da5463e66173c37c0663833f50532020346792
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-09-08 14:48:28 +0000

    A script that prints “JavaScript is amazing”:

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/*
+Write a script that prints “JavaScript is amazing”:
+
+1. You must create a constant variable called myVar with the value “JavaScript is amazing”
+2. You must use console.log(...) to print all output
+2. You are not allowed to use var
+*/
+
+const myVar = "JavaScript is amazing"
+console.log(myVar);
