0f7a8f9b5449f572dfd905dc3158e67457468b8e
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-09-10 20:22:39 +0000

    A script that prints 3 lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -8,7 +8,6 @@ Write a script that prints 3 lines:
 3. The third line: “JavaScript is amazing”
 4. You must use console.log(...) to print all output
 5. You are not allowed to use var
-
 */
 
 let first_line = "C is fun"
