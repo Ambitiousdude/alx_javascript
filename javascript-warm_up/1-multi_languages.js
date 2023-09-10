36ae3919a9249673990a062d099bf6ecf5926e37
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-09-10 19:21:48 +0000

    A script that prints 3 lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node env
+
+/* 
+Write a script that prints 3 lines:
+
+1. The first line: “C is fun”
+2. The second line: “Python is cool”
+3. The third line: “JavaScript is amazing”
+4. You must use console.log(...) to print all output
+5. You are not allowed to use var
+
+*/
+
+let first_line = "C is fun"
+let second_line = "Python is cool"
+let third_line = "Javascript is amazing"
+
+console.log(first_line)
+console.log(second_line)
+console.log(third_line)
