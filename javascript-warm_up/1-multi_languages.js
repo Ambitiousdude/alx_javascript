0c597924029b412dce4385f59ebcd34e61eb14b0
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-09-10 20:25:12 +0000

    A script that prints 3 lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -14,6 +14,6 @@ let first_line = "C is fun"
 let second_line = "Python is cool"
 let third_line = "Javascript is amazing"
 
-console.log(first_line)
-console.log(second_line)
-console.log(third_line)
+console.log(first_line);
+console.log(second_line);
+console.log(third_line);
