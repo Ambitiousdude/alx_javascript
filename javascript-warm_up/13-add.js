3f90de61ff80d91c09d46dea8dabc1779f157c8c
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-09-11 18:12:10 +0000

    A function that returns the addition of 2 integers

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/* 
+- Write a function that returns the addition of 2 integers.
+
+1. The function must be visible from outside
+2. The name of the function must be add
+3. You are not allowed to use var
+*/
+
+exports.add = function add(a, b) {
+    return a + b;
+}
