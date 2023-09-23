dfa00e8496703eb0e64e0665d6607257d637ab9e
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-09-23 14:28:16 +0000

    Write a class Rectangle that defines a rectangle with an empty object

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -13,8 +13,8 @@
 module.exports = class Rectangle{
     constructor(w, h){
         if(w > 0 && h > 0){
-            this.width = {};
-            this.height = {};
+            this.width = w;
+            this.height = h;
         }
         }
     };
