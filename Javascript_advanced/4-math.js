64446c4409e0388918d88ccacac24ffc5d67c566
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-09-30 10:14:44 +0000

    Complex Closure

---
+++ Javascript_advanced/4-math.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+/* 
+Create a function divideBy:
+
+It takes into argument firstNumber (number)
+It returns a function that takes into argument secondNumber (number)
+It returns the second number divided by the first number
+Create a function addBy:
+
+It takes into argument firstNumber (number)
+It returns a function that takes into argument secondNumber (number)
+It returns the sum of the two numbers
+Create four closures:
+
+addBy100, that uses the function addBy with the number 100
+addBy1000, that uses the function addBy with the number 1000
+divideBy10, that uses the function divideBy with the number 10
+divideBy100, that uses the function divideBy with the number 100
+Requirements:
+
+Executing the following code:
+
+console.log(addBy100(20));
+console.log(divideBy10(20));
+console.log(divideBy100(200));
+console.log(addBy1000(20));
+Should display in the console:
+
+120
+2
+2
+1020
+
+*/
+
